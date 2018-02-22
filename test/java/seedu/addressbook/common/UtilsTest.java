7e30d60d2ebd67d1afc55f0d189a024bcdeb9d96
Zhang Yijiong <zhang.yijiong@dhs.sg>
2018-02-23 00:51:51 +0800

    add isAnyNull JUnit Test

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -43,4 +43,36 @@ public class UtilsTest {
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
+
+    @Test
+    public void isAnyNull() throws Exception{
+        // empty list
+        assertNoNull();
+
+        // no null
+        assertNoNull(1);
+        assertNoNull("");
+        assertNoNull("abc");
+        assertNoNull("abc", "ab", "a");
+        assertNoNull(1, 2);
+
+        // all null
+        assertHasNull((Object)null);
+        assertHasNull(null, null);
+        assertHasNull(null, null, null);
+        
+        // some null
+        assertHasNull("", null);
+        assertHasNull(null, "abc");
+        assertHasNull(null,"ab", null);
+        assertHasNull(1, null, 2);
+    }
+
+    private void assertHasNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }
+
+    private void assertNoNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
 }
